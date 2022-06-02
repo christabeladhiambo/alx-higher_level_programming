a81d6e4e2f175b3472ac40e833bf9543fc480d58
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-06-02 07:05:48 -0700

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-num = 0
-for x in range(ord('z'), ord('a') - 1, -1):
-print("{}".format(chr(x - num)), end="")
-num = 32 if num == 0 else 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end='')
