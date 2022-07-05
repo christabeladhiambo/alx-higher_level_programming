56c6a03273d94abcb3e9632966dbf2359921a27f
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-07-05 04:41:09 -0700

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,61 +1,34 @@
-reate a class of student
+#!/usr/bin/python3
 
+"""
+Create a class of student
 """
 
 
-
-
-
 class Student:
+    """
+    Class of student
+    """
 
-	    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Init the class with value
+        Args:
+            self (class)
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        Returns: Anything
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-	        Class of student
-
-		    """
-
-
-
-		        def __init__(self, first_name, last_name, age):
-
-				        """
-
-					        Init the class with value
-
-						        Args:
-
-							            self (class)
-
-	            first_name (str): The first name of the student
-
-				                  last_name (str): The last name of the student
-
-								               age (int): The age of the student
-
-											          Returns: Anything
-
-												          """
-
-													          self.first_name = first_name
-
-														          self.last_name = last_name
-
-															          self.age = age
-
-
-
-																      def to_json(self):
-
-																	              """
-
-																		              Create a json representation of the class
-
-																			              Args:
-
-																				                  self (class)
-
-	        Returns: The new representation
-
-		        """
-
-			        return vars(self)
+    def to_json(self):
+        """
+        Create a json representation of the class
+        Args:
+            self (class)
+        Returns: The new representation
+        """
+        return vars(self)
