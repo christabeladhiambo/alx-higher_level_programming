eac6833716e4861dacb31e7e36835256ae4c3acf
christabel <christabeladhiambo312gmail.com>
2022-08-10 13:22:31 -0700

    Cities sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Create city table with primary id and FOREIGN KEY
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`
+(
+    PRIMARY KEY (`id`),
+    `id` int NOT NULL AUTO_INCREMENT,
+    `state_id` int NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`cities`(`id`)
+);
