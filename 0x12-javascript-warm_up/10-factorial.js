a839e106446d3fa7545a0d78fc5e9329282acb43
christabel <christabeladhiambo312gmail.com>
2022-09-05 08:12:04 -0700

    Warmup factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+function factorial (a) {
+  if (isNaN(a) || a === 0) {
+    return 1;
+  }
+  return a * factorial((a - 1));
+}
+
+console.log(factorial(num));
